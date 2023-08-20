ae1542dc1cd9ca66e5477de01b3329ae51476938
naymurabir <naymurrahman.abir@gmail.com>
2023-08-20 20:44:28 +0600

    purchase-button-done

--- script/index.js
+++ script/index.js
@@ -1,31 +1,17 @@
-// Coupon Field
-document.getElementById('coupon-field').addEventListener('keyup', function (event) {
-    const applyButton = document.getElementById('apply-btn')
-    // console.log(event.target.value);
-    const couponCode = event.target.value
-
-    if (couponCode === "SELL200") {
-        applyButton.style.backgroundColor = '#E527B2'
-        applyButton.removeAttribute('disabled')
-
-    } else {
-        applyButton.style.backgroundColor = '#F9A8D4'
-        applyButton.setAttribute('disabled', true)
-    }
-
-})
-
 // Apply Button Action
 document.getElementById('apply-btn').addEventListener('click', function () {
-    // Step - 3
+    // Step-3
     const discountAmount = document.getElementById('discount-amount')
     const discountedTotalAmount = document.getElementById('discount-total')
+    const couponField = document.getElementById('coupon-field').value
 
-    if (totalCost > 200) {
+    if (couponField === "SELL200") {
         const discount = (totalCost / 100) * 20
         const discountUpdate = totalCost - discount
         discountAmount.innerText = parseInt(discount).toFixed(2)
         discountedTotalAmount.innerText = discountUpdate.toFixed(2)
+    } else {
+        alert("Please input SELL200")
     }
 })
 
@@ -44,5 +30,27 @@ function handleCLikBtn(target) {
     const itemTotalPrice = document.getElementById('total-price')
     totalCost = parseInt(totalCost) + parseInt(perItemPrice)
     itemTotalPrice.innerText = totalCost.toFixed(2)
+
+    // Step-4
+    const applyButton = document.getElementById('apply-btn')
+    if (totalCost >= 200) {
+        applyButton.style.backgroundColor = '#E527B2'
+        applyButton.removeAttribute('disabled')
+    } else {
+        applyButton.style.backgroundColor = '#F9A8D4'
+        applyButton.setAttribute('disabled', true)
+    }
+
+    // Step-5
+    const purchaseButton = document.getElementById('purchase-btn')
+    if (totalCost > 0) {
+        purchaseButton.style.backgroundColor = '#E527B2'
+        purchaseButton.removeAttribute('disabled')
+        return
+    } else {
+        purchaseButton.style.backgroundColor = '#F9A8D4'
+        purchaseButton.setAttribute('disabled', true)
+        return
+    }
 
 }
